30d922b66cd7dfd80dbf0d411fd68ae30f0fe661
Alexander <av.borisov@gmail.com>
2022-05-23 16:04:08 +0300

    Update HACS and some fixes

--- custom_components/lkcomu_interrao/__init__.py
+++ custom_components/lkcomu_interrao/__init__.py
@@ -324,8 +324,8 @@ async def async_setup_entry(
                     f"идентификатор {existing_config_entry_id})"
                     if IS_IN_RUSSIA
                     else f"Same profiles retrieved by multiple configurations "
-                         f"(foreign username: {existing_api_object.username}, "
-                         f"ID: {existing_config_entry_id})"
+                    f"(foreign username: {existing_api_object.username}, "
+                    f"ID: {existing_config_entry_id})"
                 )
             )
             await hass.config_entries.async_set_disabled_by(config_entry.entry_id, DOMAIN)
@@ -357,7 +357,8 @@ async def async_setup_entry(
 
 
 async def async_reload_entry(
-    hass: HomeAssistantType, config_entry: config_entries.ConfigEntry,
+    hass: HomeAssistantType,
+    config_entry: config_entries.ConfigEntry,
 ) -> bool:
     """Reload Lkcomu InterRAO entry"""
     log_prefix = _make_log_prefix(config_entry, "setup")
@@ -369,7 +370,8 @@ async def async_reload_entry(
 
 
 async def async_unload_entry(
-    hass: HomeAssistantType, config_entry: config_entries.ConfigEntry,
+    hass: HomeAssistantType,
+    config_entry: config_entries.ConfigEntry,
 ) -> bool:
     """Unload Lkcomu InterRAO entry"""
     log_prefix = _make_log_prefix(config_entry, "setup")
