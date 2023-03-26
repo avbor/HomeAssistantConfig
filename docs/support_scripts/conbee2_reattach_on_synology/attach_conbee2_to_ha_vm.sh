562f1256ae19bc299a3a0b433d771142a10d2309
Alexander <av.borisov@gmail.com>
2023-03-26 22:42:29 +0300

    Some updates related Conbee on Syno VMM

--- docs/support_scripts/conbee2_reattach_on_synology/attach_conbee2_to_ha_vm.sh
+++ docs/support_scripts/conbee2_reattach_on_synology/attach_conbee2_to_ha_vm.sh
@@ -3,6 +3,6 @@
 HA_VM_NAME="homeassistant-main"
 HA_VM_GUID=$(/usr/syno/bin/synowebapi -s --exec api=SYNO.Virtualization.API.Guest version=1 method=list runner=admin | jq -r --arg VM_NAME $HA_VM_NAME '.data.guests | .[] | select(.guest_name==$VM_NAME) | .guest_id')
 
-/usr/local/bin/virsh detach-device $HA_VM_GUID --file /volume3/sys_files/vm/ha_usb_1cf1_0030.xml --current > /dev/null 2>&1
+/usr/local/bin/virsh detach-device $HA_VM_GUID --file /volume3/sys_files/vm/ha_usb_1cf1_0030.xml > /dev/null 2>&1 || true
 sleep 3
 /usr/local/bin/virsh attach-device $HA_VM_GUID --file /volume3/sys_files/vm/ha_usb_1cf1_0030.xml --current
