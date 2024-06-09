474513bc55b2905101ee4c4dd13922857fa8bef8
Alexander <av.borisov@gmail.com>
2024-06-09 09:46:55 +0300

    Autocommit: Config at 2024-06-09 09:46:55

--- custom_components/plant/sensor.py
+++ custom_components/plant/sensor.py
@@ -221,7 +221,8 @@ class PlantCurrentStatus(RestoreSensor):
         await super().async_added_to_hass()
         state = await self.async_get_last_state()
 
-        # We do not restore the state for these they are read from the external sensor anyway
+        # We do not restore the state for these.
+        # They are read from the external sensor anyway
         self._attr_native_value = None
         if state:
             if "external_sensor" in state.attributes:
@@ -302,8 +303,8 @@ class PlantCurrentStatus(RestoreSensor):
         if (
             self.external_sensor
             and new_state
-            and new_state is not STATE_UNKNOWN
-            and new_state is not STATE_UNAVAILABLE
+            and new_state.state != STATE_UNKNOWN
+            and new_state.state != STATE_UNAVAILABLE
         ):
             self._attr_native_value = new_state.state
             if ATTR_UNIT_OF_MEASUREMENT in new_state.attributes:
@@ -328,9 +329,6 @@ class PlantCurrentIlluminance(PlantCurrentStatus):
         self._attr_icon = ICON_ILLUMINANCE
         self._external_sensor = config.data[FLOW_PLANT_INFO].get(
             FLOW_SENSOR_ILLUMINANCE
-        )
-        _LOGGER.info(
-            "Added external sensor for %s %s", self.entity_id, self._external_sensor
         )
         self._attr_native_unit_of_measurement = LIGHT_LUX
         super().__init__(hass, config, plantdevice)
