225b9135a99731a318273dccc067bcfbe35b8db4
Alexander <av.borisov@gmail.com>
2026-07-14 13:45:23 +0300

    Autocommit: Config at 2026-07-14 13:45:23

--- custom_components/yandex_station/core/quasar_info.py
+++ custom_components/yandex_station/core/quasar_info.py
@@ -12,19 +12,24 @@ QUASAR_INFO: dict[str, list] = {
     "cucumber": ["yandex:station-midi", "Яндекс", "Станция Миди (2023)"],  # zigbee
     "chiron": ["yandex:station-duo-max", "Яндекс", "Станция Дуо Макс (2023)"],  # zigbee
     "orion": ["yandex:station-max", "Яндекс", "Станция 3 (2025)"],
+    "mango": ["yandex:jbl-link-portable", "Яндекс", "Станция Стрит (2025)"],
     # платформа Яндекс.ТВ (без облачного управления!)
     "yandexmodule": ["yandex:module", "Яндекс", "Модуль (2019)"],
     "yandexmodule_2": ["yandex:module-2", "Яндекс", "Модуль 2 (2021)"],
     "yandex_tv": ["mdi:television-classic", "Unknown", "ТВ с Алисой"],
-    # ТВ с Алисой
+    # ТВ с Алисой (неизвестные модели: ТВ Станция Про QLED, ТВ Станция MiniLED)
     "goya": ["mdi:television-classic", "Яндекс", "ТВ (2022)"],
-    "magritte": ["mdi:television-classic", "Яндекс", "ТВ Станция (2023)"],
-    "monet": ["mdi:television-classic", "Яндекс", "ТВ Станция Бейсик (2024)"],
+    "magritte": ["mdi:television-classic", "Яндекс", "ТВ Станция LED (2023)"],
+    "magritte_2": ["mdi:television-classic", "Яндекс", "ТВ Станция QLED (2025)"],
+    "monet": ["mdi:television-classic", "Яндекс", "ТВ Станция Бейсик LED (2024)"],
+    "monet_2": ["mdi:television-classic", "Яндекс", "ТВ Станция Бейсик QLED (2025)"],
+    "levitan": ["mdi:television-classic", "Яндекс", "ТВ Станция MiniLED (2026)"],
+    "malevich": ["mdi:television-classic", "Яндекс", "ТВ Станция Про MiniLED (2025)"],
     # колонки НЕ Яндекса
     "lightcomm": ["yandex:dexp-smartbox", "DEXP", "Smartbox"],
     "elari_a98": ["yandex:elari-smartbeat", "Elari", "SmartBeat"],
     "linkplay_a98": ["yandex:irbis-a", "IRBIS", "A"],
-    "wk7y": ["yandex:lg-xboom-wk7y", "LG", "XBOOM AI ThinQ WK7Y"],
+    "wk7y": ["yandex:lg-xboom-wk7y", "LG", "XBoom AI ThinQ WK7Y"],
     "prestigio_smart_mate": ["yandex:prestigio-smartmate", "Prestigio", "Smartmate"],
     "jbl_link_music": ["yandex:jbl-link-music", "JBL", "Link Music"],
     "jbl_link_portable": ["yandex:jbl-link-portable", "JBL", "Link Portable"],
@@ -45,5 +50,20 @@ def has_quasar(device: dict) -> bool:
         if info["platform"] in {"saturn", "mike", "cherry"}:
             return False  # skip non speakers
         return True
+
+    return False
+
+
+def is_tv(device: dict) -> bool:
+    if info := device.get("quasar_info"):
+        return info["platform"] in {
+            "goya",
+            "magritte",
+            "magritte_2",
+            "monet",
+            "monet_2",
+            "levitan",
+            "malevich",
+        }
 
     return False
